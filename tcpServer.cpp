38e2084aabf0e4a68037f4bacbe121dadde65633
PatrickDekker98 <Patrick_Dekker@hotmail.com>
2018-11-05 16:14:34 +0100

    changed the whole library to a winsock server

--- tcpServer.cpp
+++ tcpServer.cpp
@@ -1,158 +1,183 @@
-#include <iostream>
-#include <sys/select.h>
-#include <sys/time.h>
-#include <sys/types.h>
-#include <unistd.h>
 #include "tcpServer.hpp"
-#include <sys/socket.h>
-#include <string>
+#include <iostream>
 
-tcpServer::tcpServer(unsigned int port, clients &c):
-    port(port),
-    c(c)
+
+tcpServer::tcpServer(std::string port, client clients[6], int maxClients):
+	port(port),
+//	clients(clients),
+	maxClients(maxClients)
 {
-//    for(int i = 0; i < c.maxClients; i++){
-//        c.client[i] = 0;
-//    }
-    sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (maxClients > 6) {
+		maxClients = 6;
+	}
+	for (int i = 0; i < maxClients; i++) {
+		client c = client();
+		clients[i] = c;
+	}
 
-    if (sock < 0) {
-        std::cout << " error creating socket\n";
-       exit(1); 
-    }
-    setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, (char *)&opt, sizeof(opt));
+	intResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (intResult != 0) {
+		printf("WSAStartup failed with error: %d\n", intResult);
+		return;
+	}
+	ZeroMemory(&hints, sizeof(hints));
 
-    memset((char *) &serv_addr, '0', sizeof(serv_addr));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_protocol = IPPROTO_TCP;
+	hints.ai_flags = AI_PASSIVE;
 
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = INADDR_ANY;
-    serv_addr.sin_port = htons(port);
+	intResult = getaddrinfo(NULL, port.c_str(), &hints, &result);
 
-    if(bind(sock, (struct sockaddr *) &serv_addr, 
-                sizeof(serv_addr)) <0){
-        std::cout << "no bind\n";
-        exit(1);   
-    }
+	if (intResult != 0) {
+		printf("getaddrinfo failed with error: %d\n", intResult);
+		WSACleanup();
+		return;
+	}
 
-    for(int i = 0; i < c.maxClients; i++){
-        std::cout << c.client[i] << "\n";
-    }
+	serverSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+	if (serverSocket == INVALID_SOCKET) {
+		printf("socket failed with error: %ld\n", WSAGetLastError());
+		freeaddrinfo(result);
+		WSACleanup();
+		exit(1);
+	}
+
+	intResult = bind(serverSocket, result->ai_addr, (int)result->ai_addrlen);
+	if (intResult == SOCKET_ERROR) {
+		printf("bind failed with error: %d\n", WSAGetLastError());
+		freeaddrinfo(result);
+		closesocket(serverSocket);
+		WSACleanup();
+		exit(1);
+	}
+	unsigned long b = 1;
+	ioctlsocket(serverSocket, FIONBIO, &b);
+}
+
+tcpServer::~tcpServer()
+{
+}
+int tcpServer::acceptcl(client *nc) {
+	nc->i = sizeof(sockaddr);
+	nc->c = accept(serverSocket, (sockaddr*)&nc->addr, &nc->i);
+	std::string nm = "player";
+	if (nc->c != 0 && nc->c != SOCKET_ERROR) {
+		nc->con = true;
+		FD_ZERO(&nc->set);
+		FD_SET(nc->c, &nc->set);
+		nc->id = id;
+		id++;
+		nm += (id + '0');
+		nc->name = nm;
+		nc->dmg = 20;
+		return (true);
+	}
+	return (false);
 
 }
 
-void tcpServer::listenNewClient(){
-    fd_set readfds;
-
-    listen(sock,6);
-    FD_ZERO(&readfds);
-    FD_SET(sock, &readfds);
-    addrlen = sizeof(serv_addr);
-    max_sd = sock;
-
-    for(int i = 0; i < c.maxClients; i++){
-        sd = c.client[i];
-        if (sd > 0){
-            FD_SET(sd, &readfds);
-        }
-        if (sd > max_sd){
-            max_sd = sd;
-        }
-
-        activity = select(max_sd + 1, &readfds, NULL, NULL, NULL);
-        if ((activity < 0)&& (errno!=EINTR)){
-            std::cout << "select error\n";
-        }
-        if(FD_ISSET(sock, &readfds)){
-//            std::cout << "ok\n";
-            nSock = accept(sock, (struct sockaddr *)&serv_addr, (socklen_t*)&addrlen);
-            if(nSock < 0){
-                std::cout << "accept";
-                exit(1);
-            }
-            std::cout << nSock << "new socket\n";
-
-            for(int i = 0; i < c.maxClients; i++){
-                if(c.client[i] == 0){
-//                    getpeername(sd, (struct sockaddr *)&serv_addr, (socklen_t*)&addrlen);
-                    strcpy(c.ip[i], inet_ntoa(serv_addr.sin_addr));
-//                    c.ip[i] = inet_ntoa(serv_addr.sin_addr);
-                    c.client[i] = nSock;
-                    break;
-                }
-            }
-        
-        }
-
-//        std::cout <<sd << "\n";
-    
-    }
-//    clilen = sizeof(cli_addr);
-/*    newSock = accept(sock, (struct sockaddr *) &cli_addr, (socklen_t *)&clilen);
-    if(newSock < 0) {
-        std::cout << "no accept\n";
-    } else {
-        std::cout << "new connection!\n";
-    }*/
+void tcpServer::listenNewClients() {
+	for (int i = 0; i < maxClients; i++) {
+		if (!clients[i].con) {
+			intResult = listen(serverSocket, SOMAXCONN);
+			if (intResult == SOCKET_ERROR) {
+				printf("listen failed with error: %d\n", WSAGetLastError());
+				closesocket(serverSocket);
+				WSACleanup();
+				exit(1);
+			}
+			if (acceptcl(&clients[i])) {
+				std::cout << "ok";
+			}
+		}
+	}
 }
 
-void tcpServer::recieve(){
-    fd_set readfds;
-    addrlen = sizeof(serv_addr);
-    for ( int i = 0; i < c.maxClients; i++){
-        sd = c.client[i];
-        if (FD_ISSET(sd, &readfds)){
-            valread = read(sd, buffer, 256);
-            if(valread == 0){
-                getpeername(sd, (struct sockaddr *)&serv_addr, (socklen_t *)&addrlen);
-                std::cout << inet_ntoa(serv_addr.sin_addr);
-                c.client[i] = 0;
-            }else { 
-                std::string s(buffer);
-                std::string subs = s.substr(0, valread);
-
-//                buffer[valread] = '\0';
-                std::cout << subs;
-            }
-        } 
-    }
-    /*    memset(buffer,'0', 256);
-    n = read(newSock, buffer, 255);
-    if (n <= 0){
-        std::cout << "no msg\n";
-    } else {
-        std::string s(buffer);
-        std::string subs = s.substr(0, n);
-        std::cout << subs << "\n";
-
-    }*/
-} 
-
-void tcpServer::sendCli(int cli, const char* msg){
-//    std::cout << " ok";
-    send(cli, msg, strlen(msg), 0);
+void tcpServer::recieveCli() {
+//	std::cout << "recieved";
+	for (int i = 0; i < maxClients; i++) {
+		memset(buffer, '0', 256);
+		if (clients[i].con) {
+//			std::cout << "recieved";
+			intResult = recv(clients[i].c, buffer, bufferLen, 0);
+			if (intResult > 0) {
+//				std::cout << "recieved";
+	//			std::cout << buffer;
+				std::string s(buffer);
+				std::string subs = s.substr(0, intResult);
+				msg rc = msg(subs);
+//				std::cout << rc.waarde;
+				if ( rc.command == R_KILLED_BY) {
+					msg s1;
+					s1.command = T_KILLED_BY;
+					for (int j = 0; j < maxClients; j++) {
+						if (clients[j].id == rc.waarde){
+							s1.naam = clients[j].name;
+							sendCli(clients[i].c, s1);
+							s1.command = T_KILL_CONFIRM;
+							sendCli(clients[j].c, s1);
+						}
+					}
+				}
+			}
+		}
+	}
+/*	intResult = recv(cl, buffer, bufferLen, 0);
+	if (intResult > 0) {
+		printf("Bytes received: %d\n", intResult);
+	} else if (intResult == 0) {
+		printf("Connection closing...\n");
+	} else {
+		printf("recv failed with error: %d\n", WSAGetLastError());
+		closesocket(ClientSocket);
+		WSACleanup();
+		exit(1);
+	}*/
 }
 
-int tcpServer::getClient(const char* ip){
-    for(int i = 0; i < c.maxClients;i++ ){
-        if(c.ip[i] == ip){
-            return c.client[i];
-        }
-    }
-    return 0;
+void tcpServer::sendCli(SOCKET cli, msg m) {
+	std::string msg = m.serialize();
+	const char *cmsg = msg.c_str();
+	//std::cout <<sizeof(msg);
+	sendResult = send(cli, cmsg, strlen(cmsg), 0);
 }
 
-void tcpServer::sendAll(const char* msg){
-    for(int i = 0; i < c.maxClients; i++){
-        sendCli(c.client[i], msg);
-    }
+void tcpServer::sendAll(msg m) {
+	for (int i = 0; i < maxClients; i++) {
+		sendCli(clients[i].c, m);
+	}
 }
 
 void tcpServer::startGame() {
-//`    for
-    msg start = msg("T_START_GAME:,;");
-    const char* ms = (start.serialize().c_str());
-    sendAll(ms);
-
+	msg m;
+	m.command = T_PLAYER_ID;
+	for (int i = 0; i < maxClients; i++) {
+		m.waarde = clients[i].id;
+		sendCli(clients[i].c, m);
+	}
+	m.command = T_PLAYER_NAME;
+	for (int i = 0; i < maxClients; i++) {
+		m.naam = clients[i].name;
+		sendCli(clients[i].c, m);
+	}
+	m.command = T_SELECTED_DMG;
+	for (int i = 0; i < maxClients; i++) {
+		m.waarde = clients[i].dmg;
+		sendCli(clients[i].c, m);
+	}
+	m.command = T_START_GAME;
+	sendAll(m);
 }
 
+void tcpServer::oneMinuteLeft() {
+	msg m;
+	m.command = T_LAST_MINUTE;
+	sendAll(m);
+}
 
+void tcpServer::gameOver() {
+	msg m;
+	m.command = T_GAME_OVER;
+	sendAll(m);
+}
--- tcpServer.hpp
+++ tcpServer.hpp
@@ -1,32 +1,31 @@
-#include <iostream>
-#include <sys/select.h>
-#include <arpa/inet.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <netinet/in.h>
-#include <string.h>
-#include <unistd.h>
+#undef UNICODE
 
-class guiListener {
-    public:
-        virtual void startGame()=0;
-//        virtual void oneMinuteLeft()=0;
-//        virtual void stopGame()=0;
+#define WIN32_LEAN_AND_MEAN
 
-};
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string>
 
-enum CMD : uint8_t{
-    T_PLAYER_NAME, //0
-	T_SELECTED_DMG, //1
-	T_PLAYER_ID, //2
-	T_START_GAME, //3
-	T_KILLED_BY, //4
-	T_KILL_CONFIRM, //5
-	T_LAST_MINUTE, //6
-	T_GAME_OVER, //7
-	R_KILLED_BY, //8
-	N_DEFAULT_EMPTY //9
+// Need to link with Ws2_32.lib
+#pragma comment (lib, "Ws2_32.lib")
+// #pragma comment (lib, "Mswsock.lib")
 
+#define DEFAULT_BUFLEN 512
+
+enum CMD : uint8_t {
+	T_PLAYER_NAME,
+	T_SELECTED_DMG,
+	T_PLAYER_ID,
+	T_START_GAME,
+	T_KILLED_BY,
+	T_KILL_CONFIRM,
+	T_LAST_MINUTE,
+	T_GAME_OVER,
+	R_KILLED_BY,
+	N_DEFAULT_EMPTY
 };
 
 class msg {
@@ -44,62 +43,77 @@ public:
 	msg(std::string basic_string) {
 		int colon = basic_string.find(':');
 		int comma = basic_string.find(',');
-//		int semicolon = basic_string.find(';');
+		int semicolon = basic_string.find(';');
 
-		command = static_cast<CMD>(basic_string[colon+1] - '0');
-		if(command == T_PLAYER_NAME || command == T_KILLED_BY){
-			naam = basic_string.substr(comma+1);
-		}else{
-			waarde = static_cast<uint8_t>(basic_string[comma+1] - '0');
+		command = static_cast<CMD>(basic_string[colon + 1] - '0');
+		if (command == T_PLAYER_NAME || command == T_KILLED_BY) {
+			naam = basic_string.substr(comma + 1, semicolon - 1);
+		}
+		else {
+			waarde = static_cast<uint8_t>(basic_string[comma + 1] - '0');
 		}
 	}
 
-	std::string serialize(){
+	std::string serialize() {
 		std::string out = "CMD:";
 		out += ((uint8_t)command + '0');
 		out += ",";
-		if(command == T_PLAYER_NAME || command == T_KILLED_BY){
+		if (command == T_PLAYER_NAME || command == T_KILLED_BY) {
 			out += naam;
-		}else{
-			out += (waarde + '0');
 		}
-		out += ';';
+		else {
+			out += (waarde + '0');
+			out += ';';
+		}
 		return out;
 	}
 
 
 };
 
-struct clients{
-    int maxClients;
-    int  client[6] = {0,0,0,0,0,0};
-    char ip[6][16];
-    char names[6][15];
-    clients(int maxClients):
-        maxClients(maxClients)
-    {}
+class client {
+public:
+	bool con = 0;
+	int id;
+	sockaddr_in addr;
+	SOCKET c;
+	fd_set set;
+	std::string name;
+	uint8_t dmg;
+	int i;
+	int kills = 0;
 };
 
-class tcpServer : public guiListener{
-    private:
-        int opt = 1;
-        unsigned int port;
-        int sock, newSock, clilen, n, sd, activity, nSock, max_sd, addrlen, valread;
-        sockaddr_in serv_addr, cli_addr;
-        char buffer[256];
-        clients & c;
-    public:
-        tcpServer(unsigned int port, clients &c);
-//        ~tcpServer();
-        void listenNewClient();
-        void recieve();
-        void sendCli(int cli, const char* msg);
-        void sendAll(const char* msg);
-        int getClient(const char *ip);
-        void startGame() override;
-//        void oneMinuteLeft() override;
-//        void stopGame() override;
+class tcpServer {
+private:
+	int id = 0;
+	int maxClients;
+	WSADATA wsaData;
+	std::string port;
+	int intResult;
+	int sendResult;
 
+	SOCKET serverSocket = INVALID_SOCKET;
+	SOCKET ClientSocket = INVALID_SOCKET;
 
+	struct addrinfo *result = NULL;
+	struct addrinfo hints;
+
+	const int bufferLen = 512;
+	char buffer[512];
+
+public:
+	client clients[6];
+	bool start = false;
+	tcpServer(std::string port, client clients[6], int maxClients);
+	~tcpServer();
+	int acceptcl(client *c);
+	void listenNewClients();
+	void recieveCli();
+	void sendCli(SOCKET cli, msg m);
+	void sendAll( msg m);
+	void startGame();
+	void oneMinuteLeft();
+	void gameOver();
 };
 
